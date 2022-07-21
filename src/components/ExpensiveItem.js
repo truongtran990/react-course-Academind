e39ed78744940c78f36484f55e1c3dc620e08b8b
VanTruongUIT <tvtruong.uit@gmail.com>
2022-07-22 06:56:58 +0700

    seperate date

--- src/components/ExpensiveItem.js
+++ src/components/ExpensiveItem.js
@@ -4,10 +4,16 @@ import React from 'react';
 import './ExpensiveItem.css';
 
 const ExpensiveItem = (props) => {
-
+  const day = props.date.toLocaleString('en-US', {day: 'numeric'});
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const year = props.date.getFullYear();
   return (
     <div className='expense-item'>
-      <div>{props.date.toLocaleDateString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
