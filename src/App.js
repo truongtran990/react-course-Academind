b8fb39cc117f5ebc49584f36f70c7c20b3bef477
VanTruongUIT <tvtruong.uit@gmail.com>
2022-07-22 06:48:20 +0700

    add new more component in the react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import ExpensiveItem from "./components/ExpensiveItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      date: new Date(2022, 3, 22),
+      title: 'Protect your car',
+      amount: 14324,
+    },
+    {
+      id: 2,
+      date: new Date(2022, 3, 22),
+      title: 'Skin care',
+      amount: 1234.1234,
+    },
+    {
+      id: 3,
+      date: new Date(2022, 3, 22),
+      title: 'Clean car',
+      amount: 18734.1243,
+    },
+    {
+      id: 4,
+      date: new Date(2022, 3, 22),
+      title: 'Restore old parts',
+      amount: 12143.1243,
+    },
+  ]
   return (
     <div>
       <h1>Let's get started!</h1>
-      <ExpensiveItem />
+      {
+        expenses.map(expense => {
+          return (
+            <ExpensiveItem 
+              key={expense.id}
+              date={expense.date}
+              title={expense.title}
+              amount={expense.amount}
+            />
+          );
+        })
+      }
     </div>
   );
 }
--- src/components/ExpensiveItem.js
+++ src/components/ExpensiveItem.js
@@ -3,17 +3,14 @@ import React from 'react';
 // Apply css file for this component in this file
 import './ExpensiveItem.css';
 
-const ExpensiveItem = () => {
+const ExpensiveItem = (props) => {
 
-  const expensiveDate = new Date(2022, 6, 22);
-  const expensiveName = 'Protect your car';
-  const expensivePrice = 120.23;
   return (
     <div className='expense-item'>
-      <div>{expensiveDate.toLocaleDateString()}</div>
+      <div>{props.date.toLocaleDateString()}</div>
       <div className='expense-item__description'>
-        <h2>{expensiveName}</h2>
-        <div className='expense-item__price'>${expensivePrice}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
