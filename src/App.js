e92468f9c57e074865327ccd3c87980eb190a825
VanTruongUIT <tvtruong.uit@gmail.com>
2022-07-22 06:22:50 +0700

    write first component in reactjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpensiveItem from "./components/ExpensiveItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>Let's get started!</h1>
+      <ExpensiveItem />
     </div>
   );
 }
---
+++ src/components/ExpensiveItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ExpensiveItem = () => {
+  return (
+    <div>
+      <div>30 March</div>
+      <div>
+        <h2>Protect your car</h2>
+        <div>$20 Mil</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpensiveItem;
