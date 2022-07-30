0dde973a069a32e49c2dde81dafb19f124a582bb
VanTruongUIT <tvtruong.uit@gmail.com>
2022-07-31 00:20:16 +0700

    wrapped ErrorModal by the Card

--- user-app/src/components/Notifications/InvalidUserInputModal.js
+++ user-app/src/components/Notifications/InvalidUserInputModal.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Button from '../UI/Button';
+
+import Card from '../UI/Card';
+import classes from './InvalidUserInputModal.module.css';
 
 const InvalidUserInputModal = (props) => {
   const clickHandler = (event) => {
@@ -6,12 +10,25 @@ const InvalidUserInputModal = (props) => {
   }
   return (
     <div>
-      <div>{props.title}</div>
-      <div>{props.message}</div>
-      <button 
-        type='button'
-        onClick={clickHandler}
-      >{props.children}</button>
+      <div className={classes.backdrop} />
+        <Card className={classes.modal}>
+          <header className={classes.header}>
+            <h2>
+              {props.title}
+            </h2>
+          </header>
+          <div className={classes.content}>
+            <p>{props.message}</p>
+          </div>
+          <footer className={classes.actions}>
+            <Button
+              type='button'
+              onClick={clickHandler}
+            >
+              Ok
+            </Button>
+          </footer>
+        </Card>
     </div>
   )
 }
