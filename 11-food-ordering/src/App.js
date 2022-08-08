001c6a3afa73a803f6c0e92bd53e6fac21e7e82c
VanTruongUIT <tvtruong.uit@gmail.com>
2022-08-08 22:29:13 +0700

    make a landing page with the description of shop and list out available item

--- 11-food-ordering/src/App.js
+++ 11-food-ordering/src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
 
 import Header from './components/Header/Header';
+import Meals from './components/Layout/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
