25c9c18679c839cbc4230bfc1afe4dfa76fe8089
VanTruongUIT <tvtruong.uit@gmail.com>
2022-08-08 22:06:23 +0700

    add more comment for my code was write by me

--- 11-food-ordering/src/components/Header/Header.js
+++ 11-food-ordering/src/components/Header/Header.js
@@ -5,11 +5,15 @@ import mealsImage from '../../assets/meals.jpg';
 
 const Header = (props) => {
   return (
+    // wrapp the header inside the Fragment component
     <React.Fragment>
+    {/* start, i consider: should i separate the button and app name into 2 component -> when I create a Header component instance -> I will pass the name of app and cart button -> but 2 element are not quite used many time, resuseable so we don't need to do that. */}
+    {/* Create header for app name and button */}
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Your Cart</button>
       </header>
+      {/* Create another div for past and image as background of the app */}
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
