64d04c76683f823a25f834666225c35c5ac21bea
VanTruongUIT <tvtruong.uit@gmail.com>
2022-08-12 06:51:56 +0700

    useEffect and useCallback to optimize performance

--- 14-starting-project/src/App.js
+++ 14-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,12 +6,18 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const url = "https://swapi.dev/api/films/";
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
+    setError(null);
     try {
+      console.log('Fetching data from server...');
       const response = await fetch(url);
+      if (!response.ok) {
+        throw Error('Something went wrong!');
+      }
       const data = await response.json();
   
       const transformedData = data.results.map(movieData => {
@@ -26,8 +32,25 @@ function App() {
     } catch (error) {
       console.log('Error when fetching data from server ', url);
       console.log('Error: ', error);
+      setError(error)
     }
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
+  
+
+  let contents = <p>No movies was found to display</p>;
+  if (error) {
+    contents = <p>Something went wrong!</p>
+  }
+  if (isLoading) {
+    contents = <h4>Loading ...</h4>
+  }
+  if (movies.length > 0) {
+    contents = <MoviesList movies={movies} />;
   }
   return (
     <React.Fragment>
@@ -35,9 +58,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <h4>No movies was found to display</h4>}
-        {isLoading && <h4>Loading ...</h4>}
+        {contents}
       </section>
     </React.Fragment>
   );
