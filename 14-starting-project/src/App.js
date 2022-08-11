7c4b75c9c522124725c7f6abd4f8513aee520b75
VanTruongUIT <tvtruong.uit@gmail.com>
2022-08-12 06:12:22 +0700

    firt get request

--- 14-starting-project/src/App.js
+++ 14-starting-project/src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
 
+  const url = "https://swapi.dev/api/films/";
+  const fetchMoviesHandler = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const transformedData = data.results.map(movieData => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            releaseDate: movieData.release_date,
+            openingText: movieData.opening_crawl,
+          }
+        });
+        setMovies(transformedData);
+      })
+      .catch(error => console.log('Can not fetch data from ', url, ' with error: ', error))
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
