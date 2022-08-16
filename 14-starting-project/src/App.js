f53f0d9370965f2b2085ce6bf897b4e3590291d2
VanTruongUIT <tvtruong.uit@gmail.com>
2022-08-17 05:18:01 +0700

    handle post request to create a new movie and save to firebase

--- 14-starting-project/src/App.js
+++ 14-starting-project/src/App.js
@@ -13,22 +13,25 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-a4e82-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const fetchedMovies = [];
+
+      for (const key in data) {
+        fetchedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(fetchedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +42,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-a4e82-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),    //convert javascript object to json
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
