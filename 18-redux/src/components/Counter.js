88d45aecb00bebafb1364fa1967aa6fef52d644e
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-10 21:36:06 +0700

    extract redux store from specific component

--- 18-redux/src/components/Counter.js
+++ 18-redux/src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.css';
+import { useSelector, connect } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // use this to extract data you want to get from our store. Redux will setup subscription for this component
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
