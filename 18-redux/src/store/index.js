17676324f35f5dbe6784f79291cee19118a1108f
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-10 21:23:50 +0700

    initial project with redux

---
+++ 18-redux/src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+
+/**
+ * this function to update state based on action is passed from dispatch function
+ * @param {state} state current state
+ * @param {object} action the action specify by dispatch
+ * @returns new udpated state
+ */
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// create our redux store to manage state of react component,
+const store = createStore();
+
+export { counterReducer };
