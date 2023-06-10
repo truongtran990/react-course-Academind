040cb5ec7b1af37d1fbc54176f1c7830128af107
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-10 21:43:48 +0700

    use redux to update state in component

--- 18-redux/src/components/Counter.js
+++ 18-redux/src/components/Counter.js
@@ -1,17 +1,36 @@
-import { useSelector, connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   // use this to extract data you want to get from our store. Redux will setup subscription for this component
+  // from my understand, I think useSelector do the same way with useState.
   const counter = useSelector((state) => state.counter);
 
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch &&
+      dispatch({
+        type: "increment",
+      });
+  };
+  const decrementHandler = () => {
+    dispatch &&
+      dispatch({
+        type: "decrement",
+      });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
