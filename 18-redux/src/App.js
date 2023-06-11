848798d33dd2567730f1833355c9599b8e75687f
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-11 14:03:53 +0700

    Use various dispatch to multiple slices in redux

--- 18-redux/src/App.js
+++ 18-redux/src/App.js
@@ -1,12 +1,18 @@
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Header from "./components/Header.js";
 import Auth from "./components/Auth.js";
+import UserProfile from "./components/UserProfile.js";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </>
   );
