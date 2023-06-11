370f47ac0479930ccb36dc8a14ce422959b82547
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-11 13:22:03 +0700

    create slice with redux-toolkit

--- 18-redux/src/store/index.js
+++ 18-redux/src/store/index.js
@@ -1,6 +1,26 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 /**
  * this function to update state based on action is passed from dispatch function
