d2cb58b648eb96a50e83e22ac34f56479deb374a
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-17 15:51:50 +0700

    move logic from component> useEffect into creators action

--- 19-advance-redux/src/App.js
+++ 19-advance-redux/src/App.js
@@ -5,12 +5,9 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification.js";
+import { sendCartData } from "./store/index.js";
 
-import { cartActions } from "./store/index.js";
 let isInitialApp = true;
-
-const FIREBASE_URL =
-  "https://react-http-a4e82-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
 function App() {
   const cart = useSelector((state) => state.cart.cart);
@@ -24,46 +21,12 @@ function App() {
   How to solve this problem?
   */
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        cartActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data to firebase...",
-        })
-      );
-      const response = await fetch(`${FIREBASE_URL}carts.json`, {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        cartActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitialApp) {
       isInitialApp = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        cartActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- 19-advance-redux/src/store/index.js
+++ 19-advance-redux/src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const FIREBASE_URL =
+  "https://react-http-a4e82-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
 const initialCartState = {
   cart: {
     totalQuantity: 0,
@@ -77,6 +80,48 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      cartActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data to firebase...",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(`${FIREBASE_URL}carts.json`, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        cartActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        cartActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
